102dbedac81997243b537ff156c08aafe2325a01
enlace-de <prisypau81@gmail.com>
2024-01-15 12:53:59 -0400

    se agrgea btn agregar como aplicacion

--- assets/js/script.js
+++ assets/js/script.js
@@ -85,6 +85,34 @@ logoImage.addEventListener('click', () => {
     logoImage.classList.add('animacion');
 });
 
+// funcion agregar a pagina de inicio
+
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+});
+
+function addToHomeScreen() {
+  if (deferredPrompt) {
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
+      } else {
+        console.log('User dismissed the A2HS prompt');
+      }
+      deferredPrompt = null;
+    });
+  } else {
+    alert("Para agregar a la pantalla de inicio, ve al menú de la barra inferior y selecciona COMPARTIR (cuadrado con una flecha hacia arriba), después AGREGAR A LA PANTALLA DE INICIO, finalmente AGREGAR ubicado en la esquina superior derecha.");
+  }
+}
+
+
 
 
 
